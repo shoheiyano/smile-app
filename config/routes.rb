cfb7759572e15ba65ce7794610c9305f90c59486
shoheiyano <yspersona@yahoo.co.jp>
2020-07-02 12:38:51 +0900

    フラッシュメッセージ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'posts/index'
-  root "posts#index"
 
+  root "posts#index"
   resources :posts
+  resources :users, only: [:edit, :update]
 end
